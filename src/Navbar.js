036a88c9cfe9a2b4d3cc56f05a2ebd8707804844
HAMZA ALTAF <81860059+hamzaaltaf481@users.noreply.github.com>
2024-05-03 09:58:04 +0500

    Navbar/Home button

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,28 +1,33 @@
 // Navbar.js
-import React from 'react';
-import { useGlobalContext } from './context';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useGlobalContext } from "./context";
+import { Link } from "react-router-dom";
 const Navbar = (props) => {
-  const {amount, cart} = useGlobalContext();
+  const { amount, cart } = useGlobalContext();
 
-console.log('cart',cart);
+  console.log("cart", cart);
   return (
     <nav>
-      <div className='nav-center'>
+      <div className="nav-center">
+        <Link to="/">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+            <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
+          </svg>
+        </Link>
         <h3 className="Title font-bold text-center uppercase">{props.title}</h3>
-        <div className='nav-container'>
-          <Link to="/CartNav" >
-            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
-              <path d='M16 6v2h2l2 12H0L2 8h2V6a6 6 0 1 1 12 0zm-2 0a4 4 0 1 0-8 0v2h8V6zM4 10v2h2v-2H4zm10 0v2h2v-2h-2z' />
+        <div className="nav-container">
+          <Link to="/CartNav">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+              <path d="M16 6v2h2l2 12H0L2 8h2V6a6 6 0 1 1 12 0zm-2 0a4 4 0 1 0-8 0v2h8V6zM4 10v2h2v-2H4zm10 0v2h2v-2h-2z" />
             </svg>
           </Link>
-          <div className='amount-container'>
-            <p className='total-amount'>{amount}</p>
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
           </div>
         </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar;
